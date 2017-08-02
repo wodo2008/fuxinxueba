c3a1ad7f863087b297ede61b4f010d11a4b4bc1e
wodo2008 <liujun4719@163.com>
2017-08-02 22:34:27 +0800

    u

--- smallProgram/admin.py
+++ smallProgram/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from smallProgram.models import Company,Eec_alumni,Push_position
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Eec_alumni)
+admin.site.register(Push_position)
+
