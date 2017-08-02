d2018a5ef0afde6f92ffc0a2b53939bbb989be00
wodo2008 <liujun4719@163.com>
2017-08-02 22:19:37 +0800

    update

--- smallProgram/models.py
+++ smallProgram/models.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    cid = models.AutoField(primary_key=True)
+    logo_id = models.IntegerField
+    name = models.CharField(max_length=32)
+    info = models.TextField()
+    logo = models.ImageField()
+
+class Eec_alumni(models.Model):
+    eid = models.AutoField(primary_key=True)
+    cid = models.ForeignKey("Company",null=True,blank=True)
+    name = models.CharField(max_length=32)
+    school = models.CharField(max_length=32)
+    major = models.CharField(max_length=32)
+    head_ortrait_id = models.IntegerField
+    brief_intro = models.TextField()
+    intro = models.TextField()
+    degree = models.CharField(max_length=32)
+    interview_info = models.TextField()
+
+class Push_position(models.Model):
+    pid = models.AutoField(primary_key=True)
+    cid = models.ForeignKey("Company", null=True, blank=True)
+    icon_id = models.IntegerField
+    name = models.CharField(max_length=32)
+    area = models.CharField(max_length=32)
+    detail = models.TextField()
+    info = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    publish_time = models.TimeField()
+    keywords = models.CharField(max_length=32)
+    requirement = models.TextField()
+    pluses = models.TextField()
