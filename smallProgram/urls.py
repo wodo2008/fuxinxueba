7b734943921386272ea0250060b9869f5619c588
wodo2008 <liujun4719@163.com>
2017-08-03 21:51:26 +0800

    u

--- smallProgram/urls.py
+++ smallProgram/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^getCompanyList',views.getCompanyList),
+    url(r'^getExcAlumniList',views.getExcAlumniList),
+    url(r'^getPushPositionList',views.getPushPositionList),
+    url(r'^getExcAlumniDetail',views.getExcAlumniDetail),
+    url(r'^getPushPositionDetail',views.getPushPositionDetail),
 ]
