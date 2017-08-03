439ff5849cc01537f1cefb1636b8982ec0f418aa
wodo2008 <liujun4719@163.com>
2017-08-03 20:39:49 +0800

    u

--- smallProgram/views.py
+++ smallProgram/views.py
@@ -9,12 +9,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view(['POST','GET'])
-def getCompanyList(request,*args):
-    if request.method == 'GET':
-        for k in request.query_params:
-            dict[k] = request.query_params[k]
-    elif request.method == 'POST':
-        for k in request.data:
-            dict[k] = request.data[k]
-    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=dict['cid']))
+def getCompanyList(request,cid):
+    # if request.method == 'GET':
+    #     for k in request.query_params:
+    #         dict[k] = request.query_params[k]
+    # elif request.method == 'POST':
+    #     for k in request.data:
+    #         dict[k] = request.data[k]
+
+    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=cid))
     return HttpResponse(BJradio_list, content_type='application/json; charset=utf-8')
