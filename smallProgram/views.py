b8ac7316f127275d9517db0d426c76b4afe9f8b9
wodo2008 <liujun4719@163.com>
2017-08-03 21:04:47 +0800

    u

--- smallProgram/views.py
+++ smallProgram/views.py
@@ -6,13 +6,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def getCompanyList(request,cid):
-    # if request.method == 'GET':
-    #     for k in request.query_params:
-    #         dict[k] = request.query_params[k]
-    # elif request.method == 'POST':
-    #     for k in request.data:
-    #         dict[k] = request.data[k]
-
-    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=cid))
+def getCompanyList(request):
+    if request.method == 'GET':
+        for k in request.query_params:
+            dict[k] = request.query_params[k]
+    elif request.method == 'POST':
+        for k in request.data:
+            dict[k] = request.data[k]
+    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=dict['cid']))
     return HttpResponse(BJradio_list, content_type='application/json; charset=utf-8')
