593f020d110b1e4b0c731c3790844a17eca75b66
wodo2008 <liujun4719@163.com>
2017-08-03 21:20:19 +0800

    u

--- smallProgram/views.py
+++ smallProgram/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from smallProgram.models import Company,Eec_alumni,Push_position
 import json
 
 def getCompanyList(request):
