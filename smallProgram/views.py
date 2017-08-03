eeb9131ccaa6e13ae641f57f08db18df00ad2ba2
wodo2008 <liujun4719@163.com>
2017-08-03 21:16:13 +0800

    u

--- smallProgram/views.py
+++ smallProgram/views.py
@@ -7,11 +7,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def getCompanyList(request):
-    if request.method == 'GET':
-        for k in request.query_params:
-            dict[k] = request.query_params[k]
-    elif request.method == 'POST':
-        for k in request.data:
-            dict[k] = request.data[k]
-    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=dict['cid']))
+    # if request.method == 'GET':
+    #     for k in request.query_params:
+    #         dict[k] = request.query_params[k]
+    # elif request.method == 'POST':
+    #     for k in request.data:
+    #         dict[k] = request.data[k]
+    cid = request.GET.get('cid')
+    BJradio_list = serializers.serialize("json", Company.objects.filter(cid=cid))
     return HttpResponse(BJradio_list, content_type='application/json; charset=utf-8')
