23833379c2f4d6be47f568f27d0f4da5af0db542
wodo2008 <liujun4719@163.com>
2017-08-25 23:44:46 +0800

    u

--- qaProgram/urls.py
+++ qaProgram/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from qaProgram.migrations import views
 
 urlpatterns = [
-    url(r'^qaProgram/getCompanyList',views.get_stu_question_list),
-    
+    url(r'^smallprogram/getCompanyList',views.get_stu_question_list),
+
 
 ]
