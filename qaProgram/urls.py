8db05b1b52ccb41b6e81286f2b840ab35cde3a96
wodo2008 <liujun4719@163.com>
2017-08-26 09:17:39 +0800

    u

--- qaProgram/urls.py
+++ qaProgram/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^smallprogram/get_stu_question_list',views.get_stu_question_list),
-
-
+    url(r'^smallprogram/get_grad_question_list',views.get_grad_question_list),
+    url(r'^smallprogram/get_answer',views.get_answer),
+    url(r'^smallprogram/submit_answer',views.submit_answer),
+    url(r'^smallprogram/submit_question',views.submit_question),
 ]
