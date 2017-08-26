35aae5d65e27ba8b124c67455f9be8eed7cba2ed
wodo2008 <liujun4719@163.com>
2017-08-26 12:24:22 +0800

    u

--- qaProgram/models.py
+++ qaProgram/models.py
@@ -23,7 +23,7 @@ class Answer(models.Model):
 class GradDetail(models.Model):
     gid = models.AutoField(primary_key=True)
     #avatar = models.ForeignKey("Picture",null=True,blank=True)
-    avatar = models.ImageField(upload_to='img')
+    avatar = models.ImageField()
     name = models.CharField(max_length=32,default='')
     company = models.CharField(max_length=32,default='')
     school = models.CharField(max_length=32,default='')
