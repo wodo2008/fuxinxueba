4c7af84738499a9c9d1cc2fb1af9f31ceeacdaad
wodo2008 <liujun4719@163.com>
2017-08-26 12:39:35 +0800

    u

--- qaProgram/views.py
+++ qaProgram/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.db import models
 # Create your views here.
 from django.http import HttpResponse
 from dss.Serializer import serializer
