befceb6c4044ab8ef4f614e00f35295ea1925238
wodo2008 <liujun4719@163.com>
2017-09-06 23:03:45 +0800

    u

--- fuxinxueba/urls.py
+++ fuxinxueba/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import django
 from fuxinxueba import settings
 from smallProgram import views
 
