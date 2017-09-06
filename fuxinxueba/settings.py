b22f1e252b42b90ba8bb47bf5fe132009c0970f1
wodo2008 <liujun4719@163.com>
2017-09-06 23:13:30 +0800

    u

--- fuxinxueba/settings.py
+++ fuxinxueba/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/app/fuxinxueba/static/'
 STATIC_ROOT = '/home/app/static/'
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(__file__), '../static/').replace('\\','/'),
