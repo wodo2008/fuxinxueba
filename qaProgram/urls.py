f4dc2b6f2589ce1a3df49e272f04c6a777b46985
wodo2008 <liujun4719@163.com>
2017-09-16 10:09:59 +0800

    u

--- qaProgram/urls.py
+++ qaProgram/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     url(r'^smallprogram/getGradDetail',views.getGradDetail),
     url(r'^smallprogram/getPic',views.getPic),
     url(r'^smallprogram/get_question',views.get_question),
-    url(r'^smallprogram/testanswer',views.testanswer),
 ]
