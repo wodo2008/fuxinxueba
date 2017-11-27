9a284d9f7f6e81533309fe9fb6e7c607518e8fe0
wodo2008 <liujun4719@163.com>
2017-11-28 00:55:10 +0800

    u

--- user_phone_auth/urls.py
+++ user_phone_auth/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^userpa/getIdentiCode',views.getIdentiCode),
     url(r'^userpa/submitCode',views.submitCode),
+    url(r'^userpa/index',views.index),
 ]
