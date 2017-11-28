4db1ceb675f1894ec7346a23804cae21798ebdc8
wodo2008 <liujun4719@163.com>
2017-11-28 23:20:13 +0800

    u

--- userPhoneAuth/admin.py
+++ userPhoneAuth/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from userPhoneAuth.models import unicode_literals
+from userPhoneAuth.models import User_identi_code
 
 # Register your models here.
-admin.site.register(unicode_literals)
+admin.site.register(User_identi_code)
